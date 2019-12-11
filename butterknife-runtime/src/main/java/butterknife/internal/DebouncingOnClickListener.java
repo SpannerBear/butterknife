609f858336f0dc84a03bdf193a70b16df1dc9bd0
SpannerBear <jiajun.li@happy-seed.com>
2019-12-11 17:23:27 +0800

    变更DebouncingOnClickListener逻辑

--- butterknife-runtime/src/main/java/butterknife/internal/DebouncingOnClickListener.java
+++ butterknife-runtime/src/main/java/butterknife/internal/DebouncingOnClickListener.java
@@ -1,5 +1,7 @@
 package butterknife.internal;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 
 /**
@@ -7,17 +9,39 @@ import android.view.View;
  * same frame. A click on one button disables all buttons for that frame.
  */
 public abstract class DebouncingOnClickListener implements View.OnClickListener {
-  static boolean enabled = true;
-
-  private static final Runnable ENABLE_AGAIN = () -> enabled = true;
-
-  @Override public final void onClick(View v) {
-    if (enabled) {
-      enabled = false;
-      v.post(ENABLE_AGAIN);
-      doClick(v);
+    private static final int MSG_ID = 66;
+    static boolean enabled = true;
+    
+    private static DebouncingHandler handler;
+    
+    private static Handler getHandler() {
+        if (handler == null) {
+            handler = new DebouncingHandler();
+        }
+        return handler;
     }
-  }
-
-  public abstract void doClick(View v);
+    
+    @Override
+    public final void onClick(View v) {
+        Handler handler = getHandler();
+        if (enabled) {
+            enabled = false;
+            doClick(v);
+            handler.sendEmptyMessageDelayed(MSG_ID, 100);
+        } else {
+            if (handler.hasMessages(MSG_ID)) {
+                return;
+            }
+            handler.sendEmptyMessageDelayed(MSG_ID, 100);
+        }
+    }
+    
+    public abstract void doClick(View v);
+    
+    static class DebouncingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            enabled = true;
+        }
+    }
 }
